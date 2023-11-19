d123f9e07e8a4741bf836c75c9602ef2b344b87d
Tansim Anjum <tansimanim2646@gmail.com>
2023-11-20 03:59:39 +0600

    adding task perfectly

--- routes/task.js
+++ routes/task.js
@@ -1,16 +1,17 @@
-const { json } = require('body-parser');
 const express = require('express');
 const router = express.Router();
+const moment = require('moment');
 const fs = require('fs');
 const fileData = fs.readFileSync('./storage.txt','utf-8');
-const data = JSON.parse(fileData);
+const data = fileData.length !== 0 ? JSON.parse(fileData) : [];
 router.get('/',(req,res,next)=>{
     res.render('main.pug',{
         taskData:data
     });
 });
 router.post('/',(req,res,next)=>{
-    const newTask = {id:data.length + 1,task:req.body.task,complete:false};
+    const date = moment(req.body.date).format('Do MMMM,YYYY');
+    const newTask = {id:data.length + 1,task:req.body.task,date:date,complete:false};
     data.push(newTask);
     fs.writeFileSync('./storage.txt',JSON.stringify(data));
     res.redirect('/');
